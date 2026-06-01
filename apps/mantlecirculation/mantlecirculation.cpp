GabrielRobl/terraneoX

Dimensionalised time in input and output/logging.

apps/mantlecirculation/mantlecirculation.cpp
@@ -896,7 +896,8 @@ Result<> run( const Parameters& prm )
     compute_and_write_radial_profiles(
         eta[velocity_level], subdomain_shell_idx, domains[velocity_level], prm.io_params, timestep_initial );
 
-    ScalarType simulated_time = 0.0;
+    ScalarType simulated_time    = 0.0;
+    ScalarType simulated_time_Ma = 0.0;
 
     // We need some global h. Let's, for simplicity (does not need to be too accurate) just choose the smallest h in
     // radial direction.
@@ -990,9 +991,10 @@ Result<> run( const Parameters& prm )
             prm.physics_params.thermal_diffusivity );
         const auto dt = prm.time_stepping_params.dt_scaling * dt_stable;
 
-        logroot << "Computing dt (FCT stable) ..." << std::endl;
-        logroot << "    dt_stable:                     " << dt_stable << std::endl;
-        logroot << "=>  dt (= dt_stable * dt_scaling): " << dt << std::endl;
+        logroot << "Computing dt (FCT stable) ..."
+                << "\n";
+        logroot << "    dt_stable:                     " << dt_stable * prm.physics_params.calc_time_Ma << " Ma\n";
+        logroot << "=>  dt (= dt_stable * dt_scaling): " << dt * prm.physics_params.calc_time_Ma << " Ma" << std::endl;
 
         {
             util::Timer timer_fct_substeps( "fct_substeps" );
@@ -1073,9 +1075,13 @@ Result<> run( const Parameters& prm )
             eta[velocity_level], subdomain_shell_idx, domains[velocity_level], prm.io_params, timestep );
 
         simulated_time += prm.time_stepping_params.energy_substeps * dt;
+        simulated_time_Ma = simulated_time * prm.physics_params.calc_time_Ma;
 
-        logroot << "Simulated time: " << simulated_time << " (stopping at " << prm.time_stepping_params.t_end
-                << ", we're at " << simulated_time / prm.time_stepping_params.t_end * 100.0 << "%)" << std::endl;
+        logroot << "Simulated time: " << simulated_time_Ma << " Ma\n";
+        logroot << "  Stopping at " << prm.time_stepping_params.t_end_Ma << " Ma, "
+                << std::round( simulated_time_Ma / prm.time_stepping_params.t_end_Ma * 100.0 * 10.0 ) / 10.0
+                << "% done.\n";
+        logroot << std::endl;
 
         write_timer_tree( prm.io_params, timestep );
 

apps/mantlecirculation/src/parameters.hpp
@@ -26,8 +26,11 @@ struct MeshParameters
 
 struct PlateParameters
 {
-    int initial_plate_age = 400;
-    int final_plate_age   = 0;
+    bool apply_plate_velocities = false; // This does nothing yet
+    int  initial_plate_age      = 400;
+    int  final_plate_age        = 0;
+
+    double plate_velocity_scaling = 1.0;
 };
 struct BoundaryConditionsParameters
 {
@@ -39,8 +42,6 @@ struct BoundaryConditionsParameters
 
     VelocityBC velocity_bc_cmb     = VelocityBC::NO_SLIP;
     VelocityBC velocity_bc_surface = VelocityBC::NO_SLIP;
-
-    double plate_velocity_scaling = 1.0;
 
     // Nondimensional temperatures
     double temperature_min = 0.0;
@@ -59,7 +60,7 @@ struct ViscosityParameters
     std::string radial_profile_csv_filename  = "radial_viscosity_profile.csv";
     std::string radial_profile_radii_key     = "radii";
     std::string radial_profile_viscosity_key = "viscosity";
-    double      reference_viscosity          = 1e23;
+    double      reference_viscosity          = 1e22;
     double      viscosity                    = 1.0;
 };
 
@@ -84,7 +85,8 @@ struct PhysicsParameters
     bool   internal_heating      = false;
     double internal_heating_rate = 1.0;
 
-    double calc_cm_per_year = 1.0;
+    double calc_cm_per_year = 3e-4; // from non-dim velocity to cm/a
+    double calc_time_Ma     = 1e6;  // from non-dim time to Ma
 
     ViscosityParameters viscosity_params{};
 };
@@ -113,6 +115,7 @@ struct EnergySolverParameters
 struct TimeSteppingParameters
 {
     double dt_scaling = 0.5;
+    double t_end_Ma   = 100.0;
     double t_end      = 1.0;
 
     int max_timesteps = 10;
@@ -164,6 +167,7 @@ inline void nondimensionalise( Parameters& prm )
     auto& mesh     = prm.mesh_params;
     auto& boundary = prm.boundary_params;
     auto& devel    = prm.devel_params;
+    auto& time     = prm.time_stepping_params;
 
     // --- Domain ---
 
@@ -182,8 +186,18 @@ inline void nondimensionalise( Parameters& prm )
 
     phys.thermal_diffusivity = phys.thermal_conductivity / ( phys.reference_density * phys.specific_heat_capacity );
 
-    // Precompute conversion factor from non-dim velocities to cm/a
-    phys.calc_cm_per_year = phys.characteristic_velocity * 60 * 60 * 24 * 365 * 100;
+    // Precompute conversion factors from non-dim to dimensional quantities
+    phys.calc_cm_per_year = phys.characteristic_velocity * 60 * 60 * 24 * 365 * 100; // Velocity in cm/a
+
+    phys.calc_time_Ma = mesh.mantle_thickness_m / ( phys.calc_cm_per_year * 1e4 ); // Time in Ma
+    // Acount for plate velocity scaling
+    if ( boundary.plate_params.apply_plate_velocities )
+    {
+        phys.calc_time_Ma /= boundary.plate_params.plate_velocity_scaling;
+    }
+
+    // Nondimensionalise time
+    time.t_end = time.t_end_Ma / phys.calc_time_Ma;
 
     if ( !devel.set_nondimensional_numbers )
     {
@@ -328,7 +342,9 @@ inline util::Result< std::variant< CLIHelp, Parameters > > parse_parameters( int
             "considerations. You can scale the computed dt using this value (e.g. set to 0.5 to half the estimated dt, "
             "set to 1.0 to just use the estimated dt)." )
         ->group( "Time Discretization" );
-    add_option_with_default( app, "--t-end", parameters.time_stepping_params.t_end )->group( "Time Discretization" );
+    add_option_with_default( app, "--t-end", parameters.time_stepping_params.t_end_Ma )
+        ->group( "Time Discretization" )
+        ->description( "Final time in Ma." );
     add_option_with_default( app, "--max-timesteps", parameters.time_stepping_params.max_timesteps )
         ->group( "Time Discretization" )
         ->description(

mantleconvection/TERRA-NG

apps/mantlecirculation/mantlecirculation.cpp
@@ -344,7 +344,8 @@ Result<> run( const Parameters& prm )
     compute_and_write_radial_profiles(
         eta[velocity_level], subdomain_shell_idx, domains[velocity_level], prm.io_parameters, timestep_initial );
 
-    ScalarType simulated_time = ScalarType( 0 );
+    ScalarType simulated_time    = ScalarType( 0 );
+    ScalarType simulated_time_Ma = ScalarType( 0 );
 
     // We need some global h. Let's, for simplicity (does not need to be too accurate) just choose the smallest h in
     // radial direction.
@@ -604,9 +605,14 @@ Result<> run( const Parameters& prm )
         }
 
         simulated_time += prm.time_stepping_parameters.energy_substeps * dt;
+        simulated_time_Ma = simulated_time * prm.physics_parameters.calc_time_Ma;
 
-        logroot << "Simulated time: " << simulated_time << " (stopping at " << prm.time_stepping_parameters.t_end
-                << ", we're at " << simulated_time / prm.time_stepping_parameters.t_end * 100.0 << "%)" << std::endl;
+        logroot << "Simulated time: " << simulated_time_Ma << " Ma\n";
+        logroot << "  Stopping at " << prm.time_stepping_parameters.t_end_Ma << " Ma, "
+                << std::round( simulated_time_Ma / prm.time_stepping_parameters.t_end_Ma * 100.0 * 10.0 ) / 10.0
+                << "% done.\n";
+        logroot << std::endl;
+
         timer_timestep.stop();
 
         if ( write_output )

apps/mantlecirculation/src/parameters.hpp
@@ -51,8 +51,11 @@ struct MeshParameters
 
 struct PlateParameters
 {
-    int initial_plate_age = 400;
-    int final_plate_age   = 0;
+    bool apply_plate_velocities = false; // This does nothing yet
+    int  initial_plate_age      = 400;
+    int  final_plate_age        = 0;
+
+    double plate_velocity_scaling = 1.0;
 };
 struct BoundaryConditionsParameters
 {
@@ -64,8 +67,6 @@ struct BoundaryConditionsParameters
 
     VelocityBC velocity_bc_cmb     = VelocityBC::NO_SLIP;
     VelocityBC velocity_bc_surface = VelocityBC::NO_SLIP;
-
-    double plate_velocity_scaling = 1.0;
 
     // Nondimensional temperatures
     double temperature_min = 0.0;
@@ -181,7 +182,8 @@ struct PhysicsParameters
     bool   internal_heating      = false;
     double internal_heating_rate = 1.0;
 
-    double calc_cm_per_year = 1.0;
+    double calc_cm_per_year = 3e-4; // from non-dim velocity to cm/a
+    double calc_time_Ma     = 1e6;  // from non-dim time to Ma
 
     ViscosityParameters          viscosity_parameters{};
     InitialTemperatureParameters initial_temperature{};
@@ -255,6 +257,7 @@ enum class EnergySolverType
 struct TimeSteppingParameters
 {
     double dt_scaling = 0.5;
+    double t_end_Ma   = 100.0;
     double t_end      = 1.0;
 
     int max_timesteps = 10;
@@ -315,6 +318,7 @@ inline void nondimensionalise( Parameters& prm )
     auto& mesh     = prm.mesh_parameters;
     auto& boundary = prm.boundary_parameters;
     auto& devel    = prm.devel_parameters;
+    auto& time     = prm.time_stepping_parameters;
 
     // --- Domain ---
 
@@ -333,8 +337,18 @@ inline void nondimensionalise( Parameters& prm )
 
     phys.thermal_diffusivity = phys.thermal_conductivity / ( phys.reference_density * phys.specific_heat_capacity );
 
-    // Precompute conversion factor from non-dim velocities to cm/a
-    phys.calc_cm_per_year = phys.characteristic_velocity * 60 * 60 * 24 * 365 * 100;
+    // Precompute conversion factors from non-dim to dimensional quantities
+    phys.calc_cm_per_year = phys.characteristic_velocity * 60 * 60 * 24 * 365 * 100; // Velocity in cm/a
+
+    phys.calc_time_Ma = mesh.mantle_thickness_m / ( phys.calc_cm_per_year * 1e4 ); // Time in Ma
+    // Acount for plate velocity scaling
+    if ( boundary.plate_parameters.apply_plate_velocities )
+    {
+        phys.calc_time_Ma /= boundary.plate_parameters.plate_velocity_scaling;
+    }
+
+    // Nondimensionalise time
+    time.t_end = time.t_end_Ma / phys.calc_time_Ma;
 
     if ( !devel.set_nondimensional_numbers )
     {
@@ -580,8 +594,9 @@ inline util::Result< std::variant< CLIHelp, Parameters > > parse_parameters( int
             "considerations. You can scale the computed dt using this value (e.g. set to 0.5 to half the estimated dt, "
             "set to 1.0 to just use the estimated dt)." )
         ->group( "Time Discretization" );
-    add_option_with_default( app, "--t-end", parameters.time_stepping_parameters.t_end )
-        ->group( "Time Discretization" );
+    add_option_with_default( app, "--t-end", parameters.time_stepping_parameters.t_end_Ma )
+        ->group( "Time Discretization" )
+        ->description( "Final time in Ma." );
     add_option_with_default( app, "--max-timesteps", parameters.time_stepping_parameters.max_timesteps )
         ->group( "Time Discretization" )
         ->description(